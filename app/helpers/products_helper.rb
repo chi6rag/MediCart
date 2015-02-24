d06d974de014e39f9c2a376e750b2de1635a34c0
Chirag Aggarwal <chi6rag@gmail.com>
2015-02-24 21:40:16 +0530

    Add stock column in db, add suitable validations

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,4 +3,12 @@ module ProductsHelper
 		# format("INR %.2f", price)
 		number_to_currency price
 	end
+
+	def print_stock(stock)
+		if stock > 0
+			return content_tag(:span, stock, class: 'in_stock')
+		else
+			return "Out of Stock"
+		end
+	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
 	mount_uploader :image_url, ImageUploader
 	validates_numericality_of :price
+	validates :stock, :numericality => { :greater_than_or_equal_to => 0 }
 end
