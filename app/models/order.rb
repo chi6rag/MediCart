dc29ea59b2fc45a48b74ba6684abeb59528b04a6
Chirag Aggarwal <chi6rag@gmail.com>
2015-03-26 13:13:13 +0530

    Add total and sub total prices

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_items
+
+  def total
+  	total = 0
+  	order_items.each do |item|
+  		total += item.subtotal
+  	end
+  	total
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,8 @@ class OrderItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :order
   validates_presence_of :product_id, :order_id
+
+  def subtotal
+  	product.price * quantity
+  end
 end
