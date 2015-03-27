01b7aabec7c25ca31cee86a11e7f537522965a77
Chirag Aggarwal <chi6rag@gmail.com>
2015-03-27 15:16:45 +0530

    Remove orders with 0 quantity

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,11 +4,24 @@ module ProductsHelper
 		number_to_currency price
 	end
 
-	def print_stock(stock)
+	def print_stock(stock, requested)
+		if stock <=0
+			return "Out of Stock"
+		elsif stock < requested
+			return content_tag(:span, "Insufficient Stock (#{stock})", class: 'low_stock')
+		elsif stock > requested
+			return content_tag(:span, stock, class: 'in_stock')
+		end
+			
+
+
+
 		if stock > 0
 			return content_tag(:span, stock, class: 'in_stock')
 		else
 			return "Out of Stock"
 		end
 	end
+
+
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,7 @@ class OrderItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :order
   validates_presence_of :product_id, :order_id
+  validates :quantity, :numericality => { :greater_than => 0 }
 
   def subtotal
   	product.price * quantity
