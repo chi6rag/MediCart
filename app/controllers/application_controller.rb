5077a4be85041d7990aaa36f54a9c0202fe0572e
Chirag Aggarwal <chi6rag@gmail.com>
2015-03-28 13:33:02 +0530

    Load order when user logs in; display user's name on show page of order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def load_order
+    @order = Order.find_or_initialize_by(id: session[:order_id], user_id: session[:user_id])
+    if @order.new_record?
+      @order.status = "unsubmitted"
+      @order.save!
+      session[:order_id] = @order.id
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :orders
 	def self.find_or_create_by_auth(auth_data)
 		user = find_or_initialize_by(provider: auth_data["provider"], uid: auth_data["uid"])
 		user.name = auth_data["info"]["name"]
